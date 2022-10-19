33bd87f1955bb3a57493e8ef9a24a853f3784dbc
eri0730 <eri.0730_@docomo.ne.jp>
2022-10-20 01:44:08 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
 end
