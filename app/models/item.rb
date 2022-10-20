3ac561c24ed408040d33467caf0e932fa019d602
eri0730 <eri.0730_@docomo.ne.jp>
2022-10-20 22:56:39 +0900

    商品出品機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,10 +9,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
-  validates :name, :description, :image, presence: true
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  validates :image, :name, :description, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, message: "is invalid. Input half-width characters"}
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
-  validates :category_id, :status_id, :delivery_cost_id, :shipping_date_id, numericality: {other_than: 1, message: "can't be blank"} 
+  validates :category_id, :status_id, :delivery_cost_id, numericality: {other_than: 1, message: "can't be blank"} 
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"} 
+  validates :shipping_date_id, numericality: {other_than: 1, message: "can't be blank"} 
 
 end
