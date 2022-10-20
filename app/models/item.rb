eadd898095aafc98ac5fd1811120e0a008680cb6
eri0730 <eri.0730_@docomo.ne.jp>
2022-10-20 15:09:00 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,10 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
+  validates :name, :description, :image, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+
+  validates :category_id, :status_id, :delivery_cost_id, :shipping_date_id, numericality: {other_than: 1, message: "can't be blank"} 
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"} 
+
 end
