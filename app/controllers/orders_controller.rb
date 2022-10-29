73481af01c83790833515a0fe3bdf9d535be368f
eri0730 <eri.0730_@docomo.ne.jp>
2022-10-29 23:21:00 +0900

    フォームの情報の保存を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,5 +4,23 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_destination = OrderDestination.new
   end
-  
+
+  def create
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
+
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -12,7 +12,7 @@ class OrderDestination
   validates :item_id, presence: true
 
   def save
-    oder = Oder.create(user_id: user_id, item_id: item_id)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 
