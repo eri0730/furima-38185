35e7c99b516c2a34b8a98d0004978dc9a9adcb58
eri0730 <eri.0730_@docomo.ne.jp>
2022-11-04 01:09:03 +0900

    バリデーションの修正

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -2,6 +2,7 @@ class OrderDestination
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token   
 
+  validates :token, presence: true
   validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   validates :city, presence: true
@@ -10,7 +11,6 @@ class OrderDestination
   validates :phone_number, numericality: {only_integer: true, message: 'is invalid. Input only number'}
   validates :user_id, presence: true
   validates :item_id, presence: true
-  validates :token, presence: true
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
